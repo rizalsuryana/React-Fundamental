e11872e0bf91386dc5e21b4cddb2f48d019aa127
rizalsuryana <rizal.suryn@gmail.com>
2024-12-04 20:13:24 +0700

    Menambahkan Validasi

---
+++ src/component/DeleteButton.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+
+const DeleteButton = ({ id, onDelete }) => {
+    return <button className="contact-item__delete" onClick={()=> onDelete(id)}>X</button>
+}
+
+DeleteButton.propTypes = {
+    id          : PropTypes.number.isRequired,
+    onDelete    : PropTypes.func.isRequired
+};
+
+export default DeleteButton;
